3ce40c6eb046af26ed315743613b3a5f2a446245
sbschandel <146333341+sbschandel@users.noreply.github.com>
2024-04-26 14:26:25 +0530

    new updated code

---
+++ src/main/java/org/example/NewLoginCase.java
@@ -0,0 +1,34 @@
+package org.example;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class NewLoginCase {
+    public static void main (String[] args){
+
+        System.setProperty("Webdriver.chrome.driver", "C:\\Users\\Shashi Bhushan\\Desktop\\chrome-win64\\chrome-win64\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://moeving.club/login");
+
+        //some getCommands
+
+        String pageTitle = driver.getTitle();
+        System.out.println("Page Title :" + pageTitle);
+
+        String currentUrl = driver.getCurrentUrl();
+        System.out.println("Current URL :" + currentUrl);
+
+        String pageSource = driver.getPageSource();
+        System.out.println("Page Source :" + pageSource);
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/test/java/TestingCode/Dropdowns.java
@@ -0,0 +1,18 @@
+package TestingCode;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Dropdowns {
+    public static void main(String[] args)
+    {
+        //System.setProperty("webdriver.chrome.driver", "C:\\Users\\Shashi Bhushan\\Downloads\\chrome-win64\\chrome-win64\\chromedriver.exe");-For now no need to define
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+
+
+
+
+    }
+
+}

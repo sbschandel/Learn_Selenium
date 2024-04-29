44696e451a9a5708daeaf9827242a51ecf74fdc0
sbschandel <146333341+sbschandel@users.noreply.github.com>
2024-04-29 16:18:16 +0530

    successfully login

--- src/main/java/org/example/PageLogin.java
+++ src/main/java/org/example/PageLogin.java
@@ -3,35 +3,28 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
 import java.util.Set;
 class PageLogin {
     public static void main(String[] args) {
 
         System.setProperty("Webdriver.chrome.driver", "C:\\Users\\Shashi Bhushan\\Desktop\\chrome-win64\\chrome-win64\\chromedriver.exe");
         WebDriver driver =  new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.get("https://moeving.club/login");
         driver.getTitle();
-//        driver.findElement(By.className("btn_login")).click();
 
+        driver.findElement(By.className("btn_login")).click();
+        WebElement loginButton = driver.findElement(By.xpath("//*[@id='main-content']/div/div/div/div/div/button"));
+        loginButton.click();
 
-//        WebElement loginButton = driver.findElement(By.xpath("//*[@id='main-content']/div/div/div/div/div/button"));
-//        loginButton.click();
-
-        driver.findElement(By.cssSelector("button.btn_login")).click();
-
-//        String[] windowHandles = null;
-//        for (String windowHandle : windowHandles) {
-//            // Switch to the new window
-//            driver.switchTo().window(windowHandle);
-//        }
-//        // Now you are in the new window, perform actions as needed
-//        // For example, get the title of the new window
-//        String newWindowTitle = driver.getTitle();
+        //driver.findElement(By.cssSelector("button.btn_login")).click();
 
         Set<String> windowHandlesSet = driver.getWindowHandles();
         String[] windowHandles = windowHandlesSet.toArray(new String[windowHandlesSet.size()]);
 
-// Iterate over the window handles
+        // Iterate over the window handles
         for (String windowHandle : windowHandles) {
             // Switch to the new window
             driver.switchTo().window(windowHandle);
@@ -43,20 +36,16 @@ class PageLogin {
         emailbutton.click();
         emailbutton.sendKeys("shashi.bhushan@moeving.com");
 
-        WebElement nextButton = driver.findElement(By.id("nextButton")); // Change "nextButton" to the actual ID of your Next button
-
-        // Click the Next button
+        WebElement nextButton = driver.findElement(By.xpath("//div[@id='identifierNext']"));
         nextButton.click();
-        driver.findElement(By.xpath("//*[@id=\"identifierNext\"]/div/button/div[3]")).click();
 
+        //driver.findElement(By.xpath("//*[@id=\"identifierNext\"]/div/button/div[3]")).click();
         WebElement passwordbutton = driver.findElement(By.xpath("//*[@id=\"password\"]/div[1]/div/div[1]/input"));
         passwordbutton.click();
         passwordbutton.sendKeys("sbsmoeving1234#");
 
-
-//        WebElement nextbutton = driver.findElement(By.xpath("//*[@id=\"identifierNext\"]/div/button/div[3]"));
-//        nextbutton.click();
-
+        WebElement loginnextButton = driver.findElement(By.xpath("//div[@id='passwordNext']"));
+        loginnextButton.click();
 
     }
 }
